089173d12e70627475053be034c1a1d159aee933
= <=>
2025-04-30 12:07:54 -0400

    update users

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import (
 )
 from django.db import models
 from django.utils import timezone
+from datetime import timedelta
 
 
 class UserManager(BaseUserManager):
@@ -87,3 +88,15 @@ class Student(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} (Student)"
+
+
+class Otp(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_expired(self):
+        return timezone.now() > self.created_at + timedelta(minutes=10)
+
+    def __str__(self):
+        return f"{self.user.email} - {self.code}"
