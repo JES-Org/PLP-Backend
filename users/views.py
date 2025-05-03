063c2d316badea7015f14ea9e3855f814bf99dd1
Elianamos29 <eliasayana2@gmail.com>
2025-05-03 02:57:49 -0400

    add authentication views and urls

--- users/views.py
+++ users/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+from .models import User
+from .serializers import (
+    UserSerializer,
+    RegisterSerializer,
+    CustomTokenObtainPairSerializer,
+)
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    access = refresh.access_token
+
+    return {
+        'refresh': str(refresh),
+        'access': str(access),
+    }
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        tokens = get_tokens_for_user(user)
+
+        return Response({
+            'user': UserSerializer(user).data,
+            'tokens': tokens,
+        }, status=status.HTTP_201_CREATED)
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        try:
+            user = User.objects.get(email=request.data['email'])
+            response.data['user'] = UserSerializer(user).data
+        except User.DoesNotExist:
+            response.data['user'] = None
+        return response
