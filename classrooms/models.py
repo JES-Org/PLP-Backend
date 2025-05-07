88c3748cd6543ec9e5591303f0756d598d9d6782
jenberu <jemberu0970@gmail.com>
2025-05-08 01:16:35 +0300

    refactor: add description and timestamps to Department model for improved clarity

--- classrooms/models.py
+++ classrooms/models.py
@@ -4,6 +4,11 @@ from users.models import Teacher, Student
 
 class Department(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True,blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 
     def __str__(self):
         return self.name
