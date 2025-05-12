fa6c1d00c06c02d8cd0ffa34da9231d9cd3f470a
Elianamos29 <eliasayana2@gmail.com>
2025-05-12 11:39:59 -0400

    add publish assessment view

--- assessments/urls.py
+++ assessments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AssessmentCreateView
+from .views import AssessmentCreateView, AssessmentPublishView
 
 urlpatterns = [
     path('classroom/<str:classroom_id>/assessment/', AssessmentCreateView.as_view(), name='create-assessment'),
+    path('classroom/<str:classroom_id>/assessment/publish/<str:id>/', AssessmentPublishView.as_view(), name='publish-assessment'),
 ]
--- assessments/views.py
+++ assessments/views.py
@@ -1,4 +1,6 @@
+from datetime import timezone
 from rest_framework import generics, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
@@ -36,3 +38,45 @@ class AssessmentCreateView(generics.CreateAPIView):
             "data": serializer.data,
             "errors": []
         }, status=status.HTTP_201_CREATED)
+
+class AssessmentPublishView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, classroom_id, id):
+        try:
+            assessment = Assessment.objects.get(id=id, classroom_id=classroom_id)
+        except Assessment.DoesNotExist:
+            return Response({
+                "isSuccess": False,
+                "message": "Assessment not found",
+                "data": None,
+                "errors": ["Assessment not found for the given classroom."]
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        if assessment.is_published:
+            return Response({
+                "isSuccess": False,
+                "message": "Assessment could not be published",
+                "data": None,
+                "errors": ["Assessment is already published."]
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        if assessment.deadline < timezone.now():
+            return Response({
+                "isSuccess": False,
+                "message": "Assessment could not be published",
+                "data": None,
+                "errors": ["Assessment deadline is expired."]
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        assessment.is_published = True
+        assessment.save()
+
+        serializer = AssessmentSerializer(assessment)
+
+        return Response({
+            "isSuccess": True,
+            "message": "Assessment published successfully.",
+            "data": serializer.data,
+            "errors": []
+        }, status=status.HTTP_200_OK)
