1f7c83a2003775a23dab8efa70181756d3693a91
jenberu <jemberu0970@gmail.com>
2025-05-12 18:54:39 +0300

    fix: add reset password URL to user routes

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from .views import CustomTokenObtainPairView, GetStudentByUserIdView, GetTeacherByUserIdView, RegisterView, UpdateStudentProfileView, UpdateTeacherProfileView, send_otp, verify_otp
+from .views import( CustomTokenObtainPairView, GetStudentByUserIdView, 
+                   GetTeacherByUserIdView, RegisterView, UpdateStudentProfileView, 
+                   UpdateTeacherProfileView, send_otp, verify_otp,ResetPasswordView)
 from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path('reset-password/', ResetPasswordView.as_view(), name='reset-password'),
     path("otp/send/", send_otp, name="send-otp"),
     path("otp/verify/", verify_otp, name="verify-otp"),
     path("teacher/<str:user_id>/", GetTeacherByUserIdView.as_view(), name="get-teacher-by-user-id"),
