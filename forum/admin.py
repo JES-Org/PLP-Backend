2fb00e36eea0d7987ff24bbf61cd8ec3ea0c7dce
jenberu <jemberu0970@gmail.com>
2025-05-18 17:42:29 +0300

    feat: add admin configuration for ForumMessage model and initialize forum app

---
+++ forum/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import ForumMessage
+
+@admin.register(ForumMessage)
+class ForumMessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'classroom', 'sender', 'content', 'updatedAt')
+    search_fields = ('classroom__name', 'sender__username', 'content')
+    list_filter = ('classroom', 'sender')
+    ordering = ('-updatedAt',)
+
+
+# Register your models here.
