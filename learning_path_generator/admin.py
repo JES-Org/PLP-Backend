8a9345462f8c2719b720665c4bf7bf1f5a5976ac
jenberu <jemberu0970@gmail.com>
2025-05-21 01:09:31 +0300

    feat: add admin interface for LearningPath and ChatHistory models

---
+++ learning_path_generator/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import LearningPath, ChatHistory
+
+
+
+
+
+@admin.register(LearningPath)
+class LearningPathAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'updated_at')
+    search_fields = ('title',)
+    list_filter = ('created_at', 'updated_at')
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at'
+    list_per_page = 20
+    list_display_links = ('id', 'title')
+
+
+@admin.register(ChatHistory)
+class ChatHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'message',"is_ai_response",'created_at')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at'
+    list_per_page = 20
+    list_display_links = ('id', 'user')
+
+
