bb726b7cc51384b080ac3981fa05c0ee369d898e
jenberu <jemberu0970@gmail.com>
2025-05-22 00:41:26 +0300

    feat: update notification model and views to support multiple recipients and track read status

--- notifications/views.py
+++ notifications/views.py
@@ -6,13 +6,15 @@ from rest_framework import status
 from .models import Notification
 from .serializers import NotificationSerializer
 
-
 class UnreadNotificationsView(APIView):
     permission_classes = [IsAuthenticated]
-
     def get(self, request):
         user = request.user
-        unread_notifications = Notification.objects.filter(recipient=user, is_read=False)
+        unread_notifications = Notification.objects.filter(
+            recipients=user
+        ).exclude(
+            is_read_by=user
+        )
         serializer = NotificationSerializer(unread_notifications, many=True)
 
         return Response({
@@ -22,15 +24,14 @@ class UnreadNotificationsView(APIView):
             "errors": None
         })
 
-
 class MarkNotificationAsReadView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, notification_id):
+        user = request.user
         try:
-            notification = Notification.objects.get(id=notification_id, recipient=request.user)
-            notification.is_read = True
-            notification.save()
+            notification = Notification.objects.get(id=notification_id, recipients=user)
+            notification.is_read_by.add(user)
 
             return Response({
                 "isSuccess": True,
@@ -38,6 +39,7 @@ class MarkNotificationAsReadView(APIView):
                 "data": None,
                 "errors": None
             })
+
         except Notification.DoesNotExist:
             return Response({
                 "isSuccess": False,
@@ -45,3 +47,4 @@ class MarkNotificationAsReadView(APIView):
                 "data": None,
                 "errors": "Notification with given ID does not exist or doesn't belong to you."
             }, status=status.HTTP_404_NOT_FOUND)
+
