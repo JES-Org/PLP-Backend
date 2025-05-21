32573b2a98d55b35f25acc72477cb8856f7847bd
jenberu <jemberu0970@gmail.com>
2025-05-22 00:40:36 +0300

    feat: update NotificationAdmin to simplify list display and search fields

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from .models import Notification
 
-
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'recipient', 'sender', 'message', 'url', 'is_read', 'created_at')
-    search_fields = ('recipient__username', 'message')
+    list_display = ('id', 'sender', 'message', 'url', 'created_at')
+    search_fields = ('message',)
     ordering = ('-created_at',)
     list_per_page = 10
-    list_display_links = ('id', 'recipient')
-# Register your models here.
+    list_display_links = ('id',)
